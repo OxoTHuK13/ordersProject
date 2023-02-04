6c83c09303941b02d6f629d5ceb4abbefd41f6da
OxoTHuK13 <you@example.com>
2023-02-04 18:55:48 +0400

    Добавил в админку фильтр по ФИО для Заказов

--- orders/admin.py
+++ orders/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from orders.models import *
 
-admin.site.register(Order)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', )
+    list_filter = ('customer', 'customer__email')
+    filter_horizontal = ('tank',)
+
+
+admin.site.register(Order, OrderAdmin)
