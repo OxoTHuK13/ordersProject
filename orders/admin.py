31c58272a0230ea98e21e62adee158827bd05ed9
OxoTHuK13 <you@example.com>
2023-02-05 21:05:47 +0400

    Создал историю заказов, добавил модель ТК

--- orders/admin.py
+++ orders/admin.py
@@ -3,11 +3,18 @@ from django.contrib import admin
 from orders.models import *
 
 
+class HistoryOrderStatusInline(admin.TabularInline):
+    model = HistoryOrderStatus
+    extra = 0
+
+
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'customer', 'status',)
-    list_filter = ('customer', 'status',)
+    list_display = ('id', 'customer', 'get_status', 'get_status_date', 'delivery')
+    list_filter = ('customer',)
     filter_horizontal = ('tank',)
+    inlines = (HistoryOrderStatusInline,)
 
 
 admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderStatus)
+admin.site.register(DeliveryCompany)
--- orders/models.py
+++ orders/models.py
@@ -4,6 +4,38 @@ from customers.models import *
 from tanks.models import *
 
 
+class DeliveryCompany(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Наименование')
+
+    class Meta:
+        verbose_name = 'Транспортная компания'
+        verbose_name_plural = 'Транспортные компании'
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name='Покупатель')
+    tank = models.ManyToManyField(Tank, verbose_name='Бак')
+    comment = models.TextField(verbose_name='Комментарий', null=True, blank=True)
+    delivery = models.ForeignKey(DeliveryCompany, on_delete=models.SET_NULL, null=True, blank=True,
+                                 verbose_name='Доставка')
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return f'ID: {self.pk}, {self.customer}'
+
+    def get_status(self):
+        return HistoryOrderStatus.objects.filter(order_id=self.pk).last().order_status
+
+    def get_status_date(self):
+        return HistoryOrderStatus.objects.filter(order_id=self.pk).last()
+
+
 class OrderStatus(models.Model):
     name = models.CharField(max_length=50, verbose_name='Наименование')
 
@@ -15,19 +47,17 @@ class OrderStatus(models.Model):
         return self.name
 
 
-class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name='Покупатель')
-    tank = models.ManyToManyField(Tank, verbose_name='Бак')
-    comment = models.TextField(verbose_name='Комментарий', null=True, blank=True)
-    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True, blank=True,
-                               verbose_name='Статус заказа')
+class HistoryOrderStatus(models.Model):
+    status_date = models.DateTimeField('Время статуса', auto_now_add=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Заказ')
+    order_status = models.ForeignKey(OrderStatus, on_delete=models.CASCADE, verbose_name='Статус')
 
     class Meta:
-        verbose_name = 'Заказ'
-        verbose_name_plural = 'Заказы'
+        verbose_name = 'Время статуса'
+        verbose_name_plural = 'Время статуса'
 
     def __str__(self):
-        return f'ID: {self.pk}, {self.customer}'
+        return f'{self.status_date}'
 
     # TODO: Сделать так, чтоб у заказа сохранялась история статусов с указанием даты/времени
     # TODO: Создать модель допников
