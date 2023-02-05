ff3058513d550216bd70b38aaf16b3d88e18fc07
OxoTHuK13 <you@example.com>
2023-02-05 19:51:03 +0400

    Создал модель статусов заказов, связал с заказами

--- orders/admin.py
+++ orders/admin.py
@@ -4,9 +4,10 @@ from orders.models import *
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'customer', )
-    list_filter = ('customer', 'customer__email')
+    list_display = ('id', 'customer', 'status',)
+    list_filter = ('customer', 'status',)
     filter_horizontal = ('tank',)
 
 
 admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderStatus)
--- orders/models.py
+++ orders/models.py
@@ -4,10 +4,23 @@ from customers.models import *
 from tanks.models import *
 
 
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Наименование')
+
+    class Meta:
+        verbose_name = 'Статус заказа'
+        verbose_name_plural = 'Статусы заказов'
+
+    def __str__(self):
+        return self.name
+
+
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name='Покупатель')
     tank = models.ManyToManyField(Tank, verbose_name='Бак')
     comment = models.TextField(verbose_name='Комментарий', null=True, blank=True)
+    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True, blank=True,
+                               verbose_name='Статус заказа')
 
     class Meta:
         verbose_name = 'Заказ'
@@ -16,7 +29,7 @@ class Order(models.Model):
     def __str__(self):
         return f'ID: {self.pk}, {self.customer}'
 
-    # TODO: Создать модель со статусами заказа и добавить в форму заказа
+    # TODO: Сделать так, чтоб у заказа сохранялась история статусов с указанием даты/времени
     # TODO: Создать модель допников
     # TODO: Создать модель Производство
     # TODO: Реализовать добавление в Производство баков из заказа
